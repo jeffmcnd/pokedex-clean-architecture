432c6cd98c1765422a4960306a34f16dd5d70af1
Jeffrey McNally-Dawes <jeffmcnallydawes@gmail.com>
2018-09-13 17:50:57 -0700

    Another change to trigger a build

--- app/src/main/java/xyz/mcnallydawes/pokedex/GetPokemon.kt
+++ app/src/main/java/xyz/mcnallydawes/pokedex/GetPokemon.kt
@@ -7,7 +7,7 @@ import xyz.mcnallydawes.pokedex.source.PokemonSource
 
 class GetPokemon(private val pokemonSource: PokemonSource) : GetPokemonInteractor {
 
-// Test for Jenkins 2
+// Test for Jenkins 3
 
     override fun execute(request: GetPokemonRequest): Try<GetPokemonResponse> =
             pokemonSource.getById(request.id)
